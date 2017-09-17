ed729e85cc7ba612ce9b8e4b087899c25ddffb8a
bharani chennu <bharanic404@gmail.com>
2017-09-17 17:25:26 +0530

    Update send_tweet.py

--- dht11_sense_and_tweet/send_tweet.py
+++ dht11_sense_and_tweet/send_tweet.py
@@ -3,10 +3,12 @@ import sys
 from twython import Twython
 import time
 
-apiKey = 'NXYzXgDTU2pcVy3gCgdFQhM01'
-apiSecret = 'qayyayOlj4U0n040H6aQFJp4CT7CaTBbjGeJ0k7L0ZTJeO2X4z'
-accessToken = '4734179119-nsQiNRmcdD53nF50FUkJwzYdPypJIP4F0v7IOgV'
-accessTokenSecret = '2qs0PRG4HvFT53VdGKzC1V8Ts1PTQRKGGqy6jNPefV1m1'
+
+#change the below params of your twitter account
+apiKey = 'U2pcVdFQhM01'
+apiSecret = 'qayyayOX4z'
+accessToken = '47pJIP4Ffff0v7IOgV'
+accessTokenSecret = '2qs01'
 
 api = Twython(apiKey,apiSecret,accessToken,accessTokenSecret)
 def send_tweet(temp,humid):
